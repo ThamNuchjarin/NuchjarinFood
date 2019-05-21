201f85c8f4faa1b43a52eaa2ca4ea9b5bf751901
nuchjarin <devplmth@gmail.com>
2019-05-21 15:41:13 +0700

    Add user and password

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -11,6 +11,19 @@ class _AuthenState extends State<Authen> {
     return Image.asset('images/logo.png');
   }
 
+  Widget showUser() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'User :', hintText: 'Your user'),
+    );
+  }
+
+  Widget showPassword() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Password :', hintText: 'More 6 parameters'),
+    );
+  }
+
   Widget showTitle() {
     return Text(
       'Nuchjarin Food',
@@ -28,10 +41,20 @@ class _AuthenState extends State<Authen> {
       child: Column(
         children: <Widget>[
           showLogo(),
-          Container(margin: EdgeInsets.only(top: 50.0), child: showTitle())
+          Container(
+            margin: EdgeInsets.only(top: 50.0),
+            child: showTitle(),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: showUser(),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: showPassword(),
+          )
         ],
       ),
     ));
   }
-  
 }
