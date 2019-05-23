b9db19046e58d40ec92ed57ec5bee157e30c1ab9
nuchjarin <devplmth@gmail.com>
2019-05-23 15:20:56 +0700

    connect link but error

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -8,6 +8,9 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  final formkey = GlobalKey<FormState>();
+  String user, password;
+
   Widget showLogo() {
     return Image.asset('images/logo.png');
   }
@@ -16,12 +19,15 @@ class _AuthenState extends State<Authen> {
     return RaisedButton(
       child: Text('Sign Up'),
       onPressed: () {
+        if (formkey.currentState.validate()) {
+          formkey.currentState.save();
+        }
         print('You click Signup');
 
         // Creat Router
         var registerRouter =
             MaterialPageRoute(builder: (BuildContext context) => Register());
-            Navigator.of(context).push(registerRouter);
+        Navigator.of(context).push(registerRouter);
       },
     );
   }
@@ -36,6 +42,14 @@ class _AuthenState extends State<Authen> {
   Widget showUser() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'User :', hintText: 'Your user'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Tell user >';
+        }
+      },
+      onSaved: (String value) {
+        user = value;
+      },
     );
   }
 
@@ -43,6 +57,14 @@ class _AuthenState extends State<Authen> {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Password :', hintText: 'More 6 parameters'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Password more than 6 characters';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
     );
   }
 
@@ -58,42 +80,45 @@ class _AuthenState extends State<Authen> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomPadding: false,
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.white, Colors.pink],
-                  begin: Alignment(-1, -1))),
-          padding: EdgeInsets.only(top: 70.0),
-          alignment: Alignment(0, -1),
-          child: Column(
-            children: <Widget>[
-              showLogo(),
-              Container(
-                margin: EdgeInsets.only(top: 50.0),
-                child: showTitle(),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                child: showUser(),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                child: showPassword(),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: showSignIn(),
-                    ),
-                    Expanded(
-                      child: showSignUp(context),
-                    )
-                  ],
+        body: Form(
+          key: formkey,
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.white, Colors.pink],
+                    begin: Alignment(-1, -1))),
+            padding: EdgeInsets.only(top: 70.0),
+            alignment: Alignment(0, -1),
+            child: Column(
+              children: <Widget>[
+                showLogo(),
+                Container(
+                  margin: EdgeInsets.only(top: 50.0),
+                  child: showTitle(),
                 ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                  child: showUser(),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                  child: showPassword(),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: showSignIn(),
+                      ),
+                      Expanded(
+                        child: showSignUp(context),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
 
 class Register extends StatefulWidget {
   @override
@@ -6,12 +8,100 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final formkey = GlobalKey<FormState>();
+  String name, user, password;
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        print('You click register');
+        if (formkey.currentState.validate()) {
+          formkey.currentState.save();
+          print('name = $name, user = $user, password = $password');
+          // uploadValueToServer();
+        }
+      },
+    );
+  }
+
+  void uploadValueToServer() async {
+    String urlPHP = 'https://www.androidthai.in.th/tid/addUserUng.php?isAdd=true&Name=$name&User=$user&Password=$password';
+
+    var response = await get(urlPHP);
+    var resultString = json.decode(response.body);
+    print('resultString ==> $resultString');
+
+
+
+  }
+
+  Widget nameTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Name :', hintText: 'Type your name'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Please fill Name !!';
+        }
+      },
+      onSaved: (String value) {
+        name = value;
+      },
+    );
+  }
+
+  Widget userTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'User :', hintText: 'Type your username'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Please fill User';
+        }
+      },
+      onSaved: (String value) {
+        user = value;
+      },
+    );
+  }
+
+  Widget passwordTextFromField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password :', hintText: 'Type your password'),
+      validator: (String value) {
+        if (value.length <= 5) {
+          return 'Please fill Password more than 6 character';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
-      ),body: Text('This is body'),
+        actions: <Widget>[registerButton()],
+      ),
+      //body: Text('This is body'),
+      body: Form(
+        key: formkey,
+        child: Container(
+          padding: EdgeInsets.all(50.0),
+          child: Column(
+            children: <Widget>[
+              nameTextFromField(),
+              userTextFromField(),
+              passwordTextFromField()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
